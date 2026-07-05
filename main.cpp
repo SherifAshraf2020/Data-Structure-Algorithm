fb9426107f9df92f50915408beaf7d3c99e0780f
SherifAshraf2020 <sherif.ashraf.farooq@gmail.com>
2026-07-05 23:55:02 +0300

    initialize CMake configuration and add main entry point

---
+++ main.cpp
@@ -0,0 +1,16 @@
+//
+// Created by Sherif Ashraf on 7/5/2026.
+// Project: Data_Structure_Algorithm
+// All rights reserved.
+//
+
+#include <iostream>
+#include <string>
+#include <cassert>
+#include "ArrayList/arrayListType.h"
+using namespace std;
+
+int main() {
+    cout<<"Hello, World!" << endl;
+    return 0;
+}
