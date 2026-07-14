43356db4bae1999e11c82aa9685c2eba8f106745
SherifAshraf2020 <sherif.ashraf.farooq@gmail.com>
2026-07-14 18:57:23 +0300

    Implement clockType class for time management with validation and time manipulation methods

---
+++ ClockType/clockType.cpp
@@ -0,0 +1,90 @@
+//
+// Created by Sherif Ashraf on 7/14/2026.
+// Project: Data_Structure_Algorithm
+// All rights reserved.
+//
+
+#include <iostream>
+#include "clockType.h"
+
+using namespace std;
+
+// Constructor delegates to setTime to avoid code duplication (DRY principle)
+clockType::clockType(int hours, int minutes, int seconds)
+{
+    setTime(hours, minutes, seconds);
+}
+
+// Validates input values; sets to 0 if out of range
+void clockType::setTime(int hours, int minutes, int seconds)
+{
+    if (0 <= hours && hours < 24)
+        hr = hours;
+    else
+        hr = 0;
+
+    if (0 <= minutes && minutes < 60)
+        min = minutes;
+    else
+        min = 0;
+
+    if (0 <= seconds && seconds < 60)
+        sec = seconds;
+    else
+        sec = 0;
+}
+
+void clockType::getTime(int& hours, int& minutes, int& seconds) const
+{
+    hours = hr;
+    minutes = min;
+    seconds = sec;
+}
+
+// Prints time with leading zeros for single-digit numbers
+void clockType::printTime() const
+{
+    if (hr < 10) cout << "0";
+    cout << hr << ":";
+
+    if (min < 10) cout << "0";
+    cout << min << ":";
+
+    if (sec < 10) cout << "0";
+    cout << sec;
+}
+
+void clockType::incrementHours()
+{
+    hr++;
+    if (hr > 23)
+        hr = 0;
+}
+
+void clockType::incrementMinutes()
+{
+    min++;
+    if (min > 59)
+    {
+        min = 0;
+        incrementHours(); // Automatically increment hours
+    }
+}
+
+void clockType::incrementSeconds()
+{
+    sec++;
+    if (sec > 59)
+    {
+        sec = 0;
+        incrementMinutes(); // Automatically increment minutes
+    }
+}
+
+// Compares current object's attributes with another object's attributes
+bool clockType::equalTime(const clockType& otherClock) const
+{
+    return (hr == otherClock.hr &&
+            min == otherClock.min &&
+            sec == otherClock.sec);
+}
